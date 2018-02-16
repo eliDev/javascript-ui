42a72e532c0b6a0c09289f8aaed2e8499169a9d1
Eli Verbowski <eli@unity3d.com>
2018-02-16 15:23:29 -0800

    s

--- src.js
+++ src.js
@@ -591,11 +591,14 @@ class WindowEventManager {
   constructor() {
     this.onLoadCallbacks = [];
     this.didLoad = false;
+    this.isWatchingResize = false;
+    this.onResizeCallbacks = [];
   }
 
   init() {
     if (typeof window !== 'undefined') {
       window.addEventListener('load', this.onPageLoad.bind(this));
+      window.addEventListener("optimizedResize", this.onResize.bind(this));
     }
   }
 
@@ -611,6 +614,42 @@ class WindowEventManager {
     this.onLoadCallbacks.forEach(function(callback) {
       callback();
     });
+  }
+
+  registerForResizeEvent(callback) {
+    if (!this.isWatchingResize) {
+      WindowEventManager._startWatchingResize();
+      this.isWatchingResize = true;
+    }
+    this.onResizeCallbacks.push(callback);
+  }
+
+  onResize() {
+    var size = { width: window.innerWidth, height: window.innerHeight };
+    for (var i = 0; i < this.onResizeCallbacks.length; i++) {
+      this.onResizeCallbacks[i](size.width, size.height);
+    }
+  }
+
+  static _startWatchingResize() {
+    (function () {
+      var throttle = function (type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function () {
+          if (running) { return; }
+          running = true;
+          requestAnimationFrame(function () {
+            obj.dispatchEvent(new CustomEvent(name));
+            running = false;
+          });
+        };
+        obj.addEventListener(type, func);
+      };
+    
+      /* init - you can init any event */
+      throttle("resize", "optimizedResize");
+    })();
   }
 
 }/*jshint esversion: 6 */
